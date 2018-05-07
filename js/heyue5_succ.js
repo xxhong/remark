ac0bb8d3786128537c82ce22ae3aae6c7e5f516e
徐西宏 <xxhongdev@163.com>
2018-05-07 09:36:23 +0800

    no message

--- js/heyue5_succ.js
+++ js/heyue5_succ.js
@@ -1,22 +1,7 @@
 //传入的必须是json的FzInfo对象
 'use strict';
 var FzInfo = function(text) {
-    // if (text) {
-    //     var obj = JSON.parse(text);
-    //     this.name = obj.name;
-    //     this.money = obj.money;
-    //     this.cause = obj.cause;
-    //     this.date = obj.date;
-    //     this.mark = obj.mark;
-    //     this.type = obj.type;
-    // } else {
-    //     this.name = "";
-    //     this.money = "";
-    //     this.cause = "";
-    //     this.date = "";
-    //     this.mark = "";
-    //     this.type = "";
-    // }
+
 };
 var SampleContract = function () {
     LocalContractStorage.defineMapProperty(this, "usermap");
@@ -38,15 +23,11 @@ SampleContract.prototype = {
         fzq.type = type;
         var alldata = this.usermap.get(userads)
         if(alldata){
-            // var array = JSON.parse(alldata);
             alldata.push(fzq);
-            // var arr = JSON.stringify(array)
             this.usermap.put(userads, alldata)
         }else{
             var arr = [];
-            // arr.push("\""+JSON.stringify(fzq)+"\"");
             arr.push(fzq);
-            // var arrstr = JSON.stringify(arr)
             this.usermap.put(userads, arr)
         }
 
